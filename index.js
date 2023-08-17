b358655abec8053c4ad173f373520a3649dc9deb
raffmont <raffaele.montella@uniparthenope.it>
2023-08-17 10:34:47 +0200

    Changed from datagram centered to handlers centered

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// Import https://baconjs.github.io
-const Bacon = require('baconjs')
-
-
-// From the stalk.js file, import: toDatagram, computeChecksum, toHexString, and padd functions
+// From the stalk.js file, import: toDatagram, computeChecksum, toHexString, a>
 const {
   toDatagram,
   computeChecksum,
@@ -16,14 +12,14 @@ const path = require('path')
 // Import file system module
 const fs = require('fs')
 
-// Load the datagrams from the datagrams folder
-function loadDatagrams (app, plugin) {
+// Load the handlers from the handlers folder
+function loadHandlers (app, plugin) {
 
-  // Define where the datagrams are located
-  const fpath = path.join(__dirname, 'datagrams')
+  // Define where the handlers are located
+  const fpath = path.join(__dirname, 'handlers')
 
-  // Return the datagrams dictionary:
-  // datagram name as a key, datagram definition as value
+  // Return the handlers dictionary:
+  // handler name as a key, handler definition as value
   return fs
       // Read the directory synchronously
       .readdirSync(fpath)
@@ -34,192 +30,105 @@ function loadDatagrams (app, plugin) {
       // For each file in the directory
       .reduce((acc, fname) => {
 
-        // Get the datagram basename removing .js
-        let datagram = path.basename(fname, '.js')
+        // Import the module defining the handler
+        handler = require(path.join(fpath, fname))(app, plugin)
+	
+	// Add the enable/disable handler entry in the configuration schema
+	plugin.schema.properties[handler.path] = {
+		title: handler['title'],
+      		type: 'boolean',
+		default: false
+	}
 
-        // Import the module defining the datagram
-        acc[datagram] = require(path.join(fpath, datagram))(app, plugin)
 
-        // Return the datagram definition
+	acc[handler.path] = handler
+
+        // Return the handler definition
         return acc
       }, {})
 }
 
-// Get the throttle property name by the datagram key
-const getThrottlePropname = (key) => `${key}_throttle`
-
-// Add the datagrams to the configuration schema
-function buildSchemaFromDatagrams (plugin) {
-
-  // For each key in the datagrams dictionary...
-  Object.keys(plugin.datagrams).forEach(key => {
-
-    // Get the datagram key
-    let datagram = plugin.datagrams[key]
-
-    // Get the name of the throttle property
-    const throttlePropname = getThrottlePropname(key)
-
-    // Add the enable/disable datagram entry in the configuration schema
-    plugin.schema.properties[key] = {
-      title: datagram['title'],
-      type: 'boolean',
-      default: false
-    }
-
-    // Add the throttle datagram entry in the configuration schema
-    plugin.schema.properties[throttlePropname] = {
-      title: `${key} throttle ms`,
-      type: 'number',
-      default: datagram['throttle']
-    }
-  })
-}
-
-// Define the plugin
 module.exports = function (app) {
 
-  // Plugin data
-  let plugin = {
+	var plugin = {
+		id: 'signalk-to-stalk-plugin',
+		name: 'Convert Signal K to STALK',
+		description: 'Signal K server plugin to convert Signal K to STALK',
+		schema: {
+			type: 'object',
+    			title: 'Conversions to STALK',
+    			description: 'If there is SK data for the conversion generate the following SeaTalk datagrams',
+    			properties: {
+      				"event_identifier": {
+        				type: 'string',
+        				title: "Event identifier",
+        				default: "seatalkOut"
+      				}
+    			}
+		},
+		unsubscribes: []
+  	};
 
-    // The list of subscriptions that have to be unsubscribed when the plugin ends
-    unsubscribes: []
-  }
+	plugin.start = function (options, restartPlugin) {
+		// Here we put our plugin logic
+   		app.debug('Plugin started');
+		
+		// Load the datagrams from the datagrams directory
+		plugin.handlers = loadHandlers(app, plugin)
 
-  // Plugin in
-  plugin.id = 'sk-to-stalk'
+		 
+		var subscribes = []
+		
+		Object.keys(plugin.handlers).forEach( key => {
+			if (options[key]) {
+				subscribes.push( {
+					path: key,
+					period: plugin.handlers[key].period
+				})
+			}
+		}) 
 
-  // Plugin name
-  plugin.name = 'Convert Signal K to STALK'
+		let localSubscription = {
+    			context: 'vessels.' + app.selfId, // Get data for all contexts
+    			subscribe: subscribes
+  		};
 
-  // Plugin description
-  plugin.description = 'Plugin to convert Signal K data to STALK (SeaTalk over NMEA0183)'
+		app.subscriptionmanager.subscribe(
+			localSubscription,
+			plugin.unsubscribes,
+			subscriptionError => {
+				app.error('Error:' + subscriptionError);
+			},
+    			delta => {
+      				delta.updates.forEach(u => {
+					u.values.forEach(v => {
+					
+						if (v.path in plugin.handlers) {
+						
+							let datagrams = plugin.handlers[v.path].handler(v.value)
+							
+							datagrams.forEach( datagram => {
+								app.debug(datagram)
+								// Emit the event on the associated ST2USB device
+            							app.emit(options["event_identifier"], datagram)
 
-  // Plugin configuration schema
-  plugin.schema = {
-    type: 'object',
-    title: 'Conversions to STALK',
-    description:
-      'If there is SK data for the conversion generate the following SeaTalk datagrams from Signal K data:',
-    properties: {
-      "event_identifier": {
-        type: 'string',
-        title: "Event identifier",
-        default: "seatalkOut"
-      }
-    }
-  }
+							})
+						
+						}
+					});
+      				});
+    			}
+  		);
+	};
 
-  // Load the datagrams from the datagrams directory
-  plugin.datagrams = loadDatagrams(app, plugin)
+	plugin.stop = function () {
+		// Here we put logic we need when the plugin stops
+		app.debug('Plugin stopped');
 
-  // Add the datagrams to the configuration schema
-  buildSchemaFromDatagrams(plugin)
+		plugin.unsubscribes.forEach(f => f());
+  		plugin.unsubscribes = [];
+	};
 
-  // Define the start callback - called by the framework when the plugin starts
-  plugin.start = function (options) {
+	return plugin;
+};
 
-    // The self context
-    const selfContext = 'vessels.' + app.selfId
-
-    // Select only the delta matching with self
-    const selfMatcher = delta => delta.context && delta.context === selfContext
-
-    //
-    function mapToStalk (encoder, throttle) {
-
-        // Display a debug message
-	    app.debug(encoder.datagram)
-
-        // Define the Signal K self streams produced by the datagram keys (i.e. "navigation.position")
-        const selfStreams =
-
-            // For each datagram key
-            encoder.keys.map((key, index) => {
-
-              // Get the self stream from the framework
-              let stream = app.streambundle.getSelfStream(key)
-
-              // Check if the encoder defaults are consistent
-              if (encoder.defaults && typeof encoder.defaults[index] != 'undefined') {
-
-                // Create the stream
-                stream = stream.merge(Bacon.once(encoder.defaults[index]))
-              }
-
-              // Return the stream
-              return stream
-
-            }, app.streambundle)
-
-      // Get the datagram function handler ("g0xHH", where HH is the hex code of the datagram)
-      const sentenceEvent = encoder.datagram ? `g${encoder.datagram}` : undefined
-
-      // Define the Signal k stream associated with the datagram
-      let stream = Bacon.combineWith(function () {
-        try {
-          // Associate the datagram function handler to the stream
-          return encoder.f.apply(this, arguments)
-        } catch (e) {
-          // Show a console error
-          console.error(e.message)
-        }
-      }, selfStreams)
-          // Filter only the consistent results
-        .filter(v => typeof v !== 'undefined')
-
-          // Get only the changed values
-        .changes()
-
-          // Avoid values repeated for less than 20ms
-        .debounceImmediate(20)
-
-      // Check if throttle is defined
-      if (throttle) {
-
-        // Define the stream throttle
-        stream = stream.throttle(throttle)
-      }
-
-      // Add the datagram handler to the plugin
-      plugin.unsubscribes.push(
-        stream
-          .onValue(datagramString => {
-
-            // Emit the event on the associated ST2USB device
-            app.emit(options["event_identifier"], datagramString)
-
-            // Check if the sentence event is defined
-            if (sentenceEvent) {
-
-              // Emit the event
-              app.emit(sentenceEvent, datagramString)
-            }
-
-            // Display a debug message
-            app.debug(datagramString)
-          })
-      )
-    }
-
-    // For each key in the datagram dictionary
-    Object.keys(plugin.datagrams).forEach(name => {
-
-      // Check if the datagram is enabled
-      if (options[name]) {
-
-        // Map the datagram
-        mapToStalk(plugin.datagrams[name], options[getThrottlePropname(name)])
-      }
-    })
-  }
-
-  // Define the stop callback - called by the framework when the plugin ends
-  plugin.stop = function () {
-    // Unsubscribe each datagram handler
-    plugin.unsubscribes.forEach(f => f())
-  }
-
-  // Return the plugin to the framework
-  return plugin
-}
